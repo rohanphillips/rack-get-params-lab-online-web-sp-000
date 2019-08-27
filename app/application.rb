c7eb3e24d139bbf30f4320598833a0e8bdea8696
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-27 14:49:30 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,7 +15,7 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      If @@cart.size > 0
+      if @@cart.size > 0
         @@cart.each do |item|
           resp.write "#{item}\n"
         end
